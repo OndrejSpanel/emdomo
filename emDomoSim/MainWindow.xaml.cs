d489ba1cc91f300452c43747948501aa3cae366c
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2014-10-30 10:55:43 +0000

    RoomSimulator extracted into a separate file.

--- emDomoSim/MainWindow.xaml.cs
+++ emDomoSim/MainWindow.xaml.cs
@@ -22,98 +22,6 @@ namespace emDomoSim
   /// </summary>
   public partial class MainWindow : Window
   {
-    public class RoomSimulator : FanControlInput
-    {
-      int dayInYear_;
-      float timeOfDay_; //  in hours
-
-      FanControl fan_ = new FanControlPrograms.FanControlThermostat();
-      WeatherSim weatherSim_ = new WeatherSim();
-      State state_ = new State();
-
-      public RoomSimulator()
-      {
-      }
-
-      public void SelectFanControl(string fcName)
-      {
-        var fullFcName = "emDomoSim.FanControlPrograms." + fcName;
-        var fanControl = Activator.CreateInstance(Type.GetType(fullFcName));
-        if (fanControl is FanControl)
-        {
-          fan_ = (FanControl)fanControl;
-        }
-      }
-      public class State : WeatherSim.Weather
-      {
-        public float roomTemperature_;
-
-        public State()
-        {
-          roomTemperature_ = 15;
-        }
-        public State(State w)
-          : base(w)
-        {
-          roomTemperature_ = w.roomTemperature_;
-        }
-
-      }
-
-      public float SetTime(int dayInYear, float timeOfDay)
-      {
-        const float maxDelta = 0.2f;
-        float delta = timeOfDay - timeOfDay_;
-        if (dayInYear_ != dayInYear) delta = 0;
-        else if (delta < 0) delta = 0;
-        else if (delta > maxDelta) delta = maxDelta;
-        dayInYear_ = dayInYear;
-        timeOfDay_ = timeOfDay;
-        return delta;
-      }
-
-      public State AdvanceTime(float deltaT)
-      {
-        const float oneDay = 24.0f;
-        var ret = Simulate(deltaT);
-        timeOfDay_ += deltaT;
-        while (timeOfDay_ > oneDay)
-        {
-          timeOfDay_ -= oneDay;
-          dayInYear_++;
-          if (dayInYear_ >= 366) dayInYear_ = 0;
-        }
-        return ret;
-      }
-
-      public State Simulate(float deltaT)
-      {
-        WeatherSim.Weather weather = weatherSim_.Simulate(dayInYear_, timeOfDay_);
-        State result = new State(state_);
-        result.SetWeather(weather);
-        // simulate how the room responds
-        float houseTemperature = 20;
-        float houseWeight = 0.9f;
-        float ambientTemperature = (weather.curTemp * (1 - houseWeight) + houseTemperature * houseWeight);
-        result.roomTemperature_ += (ambientTemperature - result.roomTemperature_) * 0.007f * deltaT;
-        state_ = result;
-        fan_.Simulate(deltaT, this);
-        if (fan_.FanStatus())
-        {
-          result.roomTemperature_ += (weather.curTemp - result.roomTemperature_) * 0.02f * deltaT;
-        }
-        return result;
-      }
-
-      public float GetOutsideTemperature() { return state_.curTemp; }
-      public float GetRoomTemperature() { return state_.roomTemperature_; }
-
-      internal bool FanStatus()
-      {
-        return fan_.FanStatus();
-      }
-    };
-
     RoomSimulator room_ = new RoomSimulator();
 
     AutoResetEvent cancel_ = new AutoResetEvent(false);
@@ -205,10 +113,7 @@ namespace emDomoSim
       var room = new RoomSimulator();
 
       var fanControlName = fanControlProgram.Text;
-      if (!String.IsNullOrEmpty(fanControlName))
-      {
-        room.SelectFanControl(fanControlProgram.Text);
-      }
+      room.SelectFanControl(fanControlProgram.Text);
       
       room.SetTime(dayInYear, 0);
       for (float t = 0; t < 24.0f * warmUpDays; t += deltaT)
---
+++ emDomoSim/RoomSimulator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emDomoSim
+{
+  public class RoomSimulator : FanControlInput
+  {
+    int dayInYear_;
+    float timeOfDay_; //  in hours
+
+    FanControl fan_ = new FanControlPrograms.FanControlThermostat();
+    WeatherSim weatherSim_ = new WeatherSim();
+    State state_ = new State();
+
+    public RoomSimulator()
+    {
+    }
+
+    public void SelectFanControl(string fcName)
+    {
+      if (String.IsNullOrEmpty(fcName)) return;
+      var fullFcName = "emDomoSim.FanControlPrograms." + fcName;
+      var fanControl = Activator.CreateInstance(Type.GetType(fullFcName));
+      if (fanControl is FanControl)
+      {
+        fan_ = (FanControl)fanControl;
+      }
+    }
+    public class State : WeatherSim.Weather
+    {
+      public float roomTemperature_;
+
+      public State()
+      {
+        roomTemperature_ = 15;
+      }
+      public State(State w)
+        : base(w)
+      {
+        roomTemperature_ = w.roomTemperature_;
+      }
+
+    }
+
+    public float SetTime(int dayInYear, float timeOfDay)
+    {
+      const float maxDelta = 0.2f;
+      float delta = timeOfDay - timeOfDay_;
+      if (dayInYear_ != dayInYear) delta = 0;
+      else if (delta < 0) delta = 0;
+      else if (delta > maxDelta) delta = maxDelta;
+      dayInYear_ = dayInYear;
+      timeOfDay_ = timeOfDay;
+      return delta;
+    }
+
+    public State AdvanceTime(float deltaT)
+    {
+      const float oneDay = 24.0f;
+      var ret = Simulate(deltaT);
+      timeOfDay_ += deltaT;
+      while (timeOfDay_ > oneDay)
+      {
+        timeOfDay_ -= oneDay;
+        dayInYear_++;
+        if (dayInYear_ >= 366) dayInYear_ = 0;
+      }
+      return ret;
+    }
+
+    public State Simulate(float deltaT)
+    {
+      WeatherSim.Weather weather = weatherSim_.Simulate(dayInYear_, timeOfDay_);
+      State result = new State(state_);
+      result.SetWeather(weather);
+      // simulate how the room responds
+      float houseTemperature = 20;
+      float houseWeight = 0.9f;
+      float ambientTemperature = (weather.curTemp * (1 - houseWeight) + houseTemperature * houseWeight);
+      result.roomTemperature_ += (ambientTemperature - result.roomTemperature_) * 0.007f * deltaT;
+      state_ = result;
+      fan_.Simulate(deltaT, this);
+      if (fan_.FanStatus())
+      {
+        result.roomTemperature_ += (weather.curTemp - result.roomTemperature_) * 0.02f * deltaT;
+      }
+      return result;
+    }
+
+    public float GetOutsideTemperature() { return state_.curTemp; }
+    public float GetRoomTemperature() { return state_.roomTemperature_; }
+
+    internal bool FanStatus()
+    {
+      return fan_.FanStatus();
+    }
+  };
+
+}

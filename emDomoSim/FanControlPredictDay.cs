62cff9a6f323f86a0541cb46648e9926bb675725
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2014-10-31 12:17:04 +0000

    Predict day introduced (dummy, from always on).

---
+++ emDomoSim/FanControlPredictDay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emDomoSim
+{
+  namespace FanControlPrograms
+  {
+    public class FanControlPredictDay: FanControl
+    {
+      public void Simulate(float deltaT, FanControlInput input) { }
+      public bool FanStatus() { return true; }
+      public string Name() { return "Predict Day"; }
+    }
+
+  }
+}

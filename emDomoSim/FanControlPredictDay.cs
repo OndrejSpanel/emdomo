7540a965d7418965cb370705e68cfd9c50d83a3a
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2014-11-04 09:40:38 +0000

    Started work on day prediction.

--- emDomoSim/FanControlPredictDay.cs
+++ emDomoSim/FanControlPredictDay.cs
@@ -10,8 +10,42 @@ namespace emDomoSim
   {
     public class FanControlPredictDay: FanControl
     {
-      public void Simulate(float deltaT, FanControlInput input) { }
-      public bool FanStatus() { return true; }
+      /// daytime (in hours)
+      float currDayTime;
+
+      float sumTemp;
+      float sumDuration;
+
+      public FanControlPredictDay()
+      {
+        sumTemp = 0;
+        sumDuration = 0;
+      }
+      public void Simulate(float deltaT, FanControlInput input)
+      {
+        var temp = input.GetOutsideTemperature();
+        // plot a curve, try to fit the history
+        if (sumDuration + deltaT >= 1)
+        {
+          float deltaTIn = 1-sumDuration;
+          float deltaTOut = sumDuration + deltaT - 1;
+          sumTemp += temp * deltaTIn;
+          float avgTemp = sumTemp;
+          sumTemp = deltaTOut;
+          sumDuration = 0;
+        }
+        else
+        {
+          sumTemp += temp*deltaT;
+          sumDuration += deltaT;
+
+        }
+        {
+          float avgTemp = sumTemp / sumDuration;
+
+        }
+      }
+      public bool FanStatus() { return currDayTime<6; }
       public string Name() { return "Predict Day"; }
     }
 

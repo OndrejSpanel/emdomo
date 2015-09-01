4b21ce8e4ac7baf83c0e68ecd75088f4db1f2353
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2015-09-01 14:14:19 +0200

    Added heat sources.

--- AccuTankSim/src/main/scala/name/spanel/emdomo/accutank/Tank.scala
+++ AccuTankSim/src/main/scala/name/spanel/emdomo/accutank/Tank.scala
@@ -1,8 +1,26 @@
 package name.spanel.emdomo.accutank
 
-class Tank(val levelCount:Int, initTemp:Float) {
-  val level = Vector.fill(levelCount)(initTemp)
+object Tank {
+  type HeatSource = () => Float
 
-  def topTemperature = level.head
-  def botTemperature = level.last
+  case class HeatSourceList(sources: Vector[(Int, HeatSource)]) {
+    def add(pos: Int, heatSource: HeatSource) = copy(sources = sources :+ pos -> heatSource)
+  }
+}
+
+import Tank._
+
+class Tank(val levels: Vector[Float], val heatSources: HeatSourceList) {
+  def levelCount = levels.size
+
+  def this(levelCount:Int, initTemp:Float) = this(Vector.fill(levelCount)(initTemp), HeatSourceList(Vector()))
+
+  def copy(levels: Vector[Float] = levels, heatSources: HeatSourceList = heatSources) = {
+    new Tank(levels, heatSources)
+  }
+
+  def topTemperature = levels.head
+  def botTemperature = levels.last
+
+  def addHeatSource(pos: Int, heatSource: HeatSource) = copy(heatSources = heatSources.add(pos,heatSource))
 }

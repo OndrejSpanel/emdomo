6c273186f51f52ee196cadf6e0542efdb00cfc86
OndrejSpanel <OndrejSpanel@users.noreply.github.com>
2015-09-01 14:49:40 +0200

    Started heating implementation.

--- AccuTankSim/src/main/scala/name/spanel/emdomo/accutank/Tank.scala
+++ AccuTankSim/src/main/scala/name/spanel/emdomo/accutank/Tank.scala
@@ -1,31 +1,47 @@
 package name.spanel.emdomo.accutank
 
 object Tank {
+  /// returns power in Watts
   type HeatSource = () => Float
 
   case class HeatSourceList(sources: Vector[(Int, HeatSource)]) {
     def add(pos: Int, heatSource: HeatSource) = copy(sources = sources :+ pos -> heatSource)
   }
+
+  val kcal = 4184.0f // https://en.wikipedia.org/wiki/Calorie
 }
 
 import Tank._
 
-class Tank(val levels: Vector[Float], val heatSources: HeatSourceList) {
-  def levelCount = levels.size
+class Tank(val mass: Float, val levelTemp: Vector[Float], val heatSources: HeatSourceList) {
+  def levelCount = levelTemp.size
+  def bottomLevel = levelTemp.size - 1
+  def levelMass = mass / levelCount
 
-  def this(levelCount:Int, initTemp:Float) = this(Vector.fill(levelCount)(initTemp), HeatSourceList(Vector()))
+  def this(mass: Float, levelCount:Int, initTemp:Float) = this(mass, Vector.fill(levelCount)(initTemp), HeatSourceList(Vector()))
 
-  def copy(levels: Vector[Float] = levels, heatSources: HeatSourceList = heatSources) = {
-    new Tank(levels, heatSources)
+  def copy(mass: Float = mass, levelTemp: Vector[Float] = levelTemp, heatSources: HeatSourceList = heatSources) = {
+    new Tank(mass, levelTemp, heatSources)
   }
 
-  def topTemperature = levels.head
-  def botTemperature = levels.last
+  def topTemperature = levelTemp.head
+  def botTemperature = levelTemp.last
 
   def addHeatSource(pos: Int, heatSource: HeatSource) = copy(heatSources = heatSources.add(pos,heatSource))
 
+  private def setTemp(pos: Int, temp: Float) = copy(levelTemp = levelTemp.patch(pos, Seq(temp), 1))
+
+  private def simulateHeatSource(pos: Int, heatSource: HeatSource)(implicit deltaT: Float)= {
+    val power = heatSource()
+    val resTemp = levelTemp(pos) + power * deltaT / kcal
+    setTemp(pos, resTemp)
+  }
   private def simulateHeatSources(implicit deltaT: Float) = {
-    this
+    var ret = this
+    for (hs <- heatSources.sources) {
+      ret = ret.simulateHeatSource(hs._1, hs._2)
+    }
+    ret
   }
   private def simulateCirculation(implicit deltaT: Float) = {
     this
--- AccuTankSim/src/test/scala/name/spanel/emdomo/accutank/TankTest.scala
+++ AccuTankSim/src/test/scala/name/spanel/emdomo/accutank/TankTest.scala
@@ -10,16 +10,18 @@ class TankTest extends FlatSpec with Matchers {
 
   "Tank" can "be created" in {
     val initTemp = 60.0f
-    val tank = new Tank(1, initTemp)
+    val tank = new Tank(100, 1, initTemp)
     tank.levelCount shouldBe 1
+    tank.bottomLevel shouldBe 0
     tank.topTemperature shouldBe initTemp
-    tank.topTemperature shouldBe initTemp
+    tank.botTemperature shouldBe initTemp
   }
 
   it should "be stable" in {
     val initTemp = 60.0f
-    var tank = new Tank(10, initTemp)
     val deltaT = 0.1f
+
+    var tank = new Tank(100, 10, initTemp)
     for (i <- 0 until 100) {
       tank = tank.simulate(deltaT)
     }
@@ -27,9 +29,16 @@ class TankTest extends FlatSpec with Matchers {
     tank.botTemperature shouldBe initTemp
   }
 
-  it should "be heated by a source" in {
+  it should "be heated by a bottom permanent source" in {
     val initTemp = 30.0f
-    val tank = new Tank(1, initTemp)
+    val deltaT = 0.1f
 
+    var tank = new Tank(100, 10, initTemp)
+    tank = tank.addHeatSource(tank.bottomLevel, () => 1000)
+    for (i <- 0 until 100) {
+      tank = tank.simulate(deltaT)
+    }
+    tank.botTemperature should be > 30.0f
+    //tank.botTemperature shouldBe 32.0f +- 1.0f
   }
 }
